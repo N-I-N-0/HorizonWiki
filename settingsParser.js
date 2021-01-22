00449e505ac263571bb785d7e1b17bec12215670
Niklas Witt <niklas_w@gmx.de>
2021-01-22 12:20:01 +0100

    Update settingsParser.js

--- settingsParser.js
+++ settingsParser.js
@@ -6,19 +6,31 @@ var dualBoxes = [];
 var checkBoxes = [];
 var lists = [];
 var values = [];
-//var externals = [];
+var externals = [];
 
+var div = document.getElementsByClassName("spriteSettings")[0];
+var xml = div.innerHTML;
+div.innerHTML="";
+var reggieDiv = document.createElement('div');
+reggieDiv.classList.add('reggieDiv');
+div.appendChild(reggieDiv);
+$(div).append(
+	$('<input>', {
+		type: 'text',
+		val: '0000 0000 0000 0000',
+		id: 'rawReggieInput',
+		//pattern: '[a-fA-F\d]+',
+		onchange: 'updateItAllOnChange()',
+		onkeyup: 'updateItAllOnChange()'
+	})
+);
+var rawInput = document.getElementById("rawReggieInput");
+rawInput.style.borderColor = "black"; 
+rawInput.style.borderStyle = "solid"; 
 
 function parseThoseSettings() {
 	//load settings into a var, remove text from page and prepare for adding usable settings
-	var div = document.getElementsByClassName("spriteSettings")[0];
-	var xml = div.innerHTML;
-	div.innerHTML="";
-	var reggieDiv = document.createElement('div');
-	reggieDiv.classList.add('reggieDiv');
-	div.appendChild(reggieDiv)
-
-
+	
 
 
 	addDualBox();
@@ -39,3 +51,20 @@ function addDualBox(text1 = "", text2 = "", initial = 0) {
 	dualBoxes.push(dualBox);
 }
 
+
+
+
+
+function updateItAllOnChange() {
+	if (rawInput.value.replace(/\s+/g, '').length == 16) {
+		let re = /[0-9A-Fa-f]{16}/g
+		if(!re.test(rawInput.value.replace(/\s+/g, ''))) {
+			rawInput.style.borderColor = "red";
+		} else {
+			rawInput.style.borderColor = "black";
+		}
+	} else {
+		rawInput.style.borderColor = "red";
+	}
+	rawInput.style.borderStyle = "solid"; 
+}
